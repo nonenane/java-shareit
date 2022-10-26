11a5db9df8f915565033dcbd2e171bbfefe14dc4
Georgiy Sporykhin <nonenane@gmail.com>
2022-10-26 23:19:42 +0300

    docker fix

--- server/src/main/java/ru/practicum/shareit/item/dto/ItemDtoForOwner.java
+++ server/src/main/java/ru/practicum/shareit/item/dto/ItemDtoForOwner.java
@@ -2,6 +2,7 @@ package ru.practicum.shareit.item.dto;
 
 import lombok.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
@@ -23,6 +24,16 @@ public class ItemDtoForOwner {
     public static class Booking {
         private Long id;
         private Long bookerId;
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class CommentDto {
+        Long id;
+        String text;
+        String authorName;
+        LocalDateTime created;
     }
 }
 
